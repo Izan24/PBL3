6bd06d34d21491860869068ba4521227b1c63e5f
Izan24 <izan.arcos@alumni.mondragon.edu>
2021-01-24 00:43:03 +0100

    pasos arreglados

--- BChef/src/eus/healthit/bchef/core/view/panels/center/CenterViewStep.java
+++ BChef/src/eus/healthit/bchef/core/view/panels/center/CenterViewStep.java
@@ -244,7 +244,7 @@ public class CenterViewStep extends JPanel {
 	private void updateView(RecipeStep step) {
 		instruction.setText(step.getText());
 		imageLabel.setIcon(new ImageIcon(step.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH)));
-		titleLabel.setText("Paso Nº " + step.getId());
+		titleLabel.setText("Paso Nº " + step.getNum());
 
 //		try {
 //			if (step.getDuration().toMillis() != 0) {
